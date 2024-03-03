7c14d6efe3b7fa93ee88db3f21c7fc01494f4f59
Gressling <thorsten.gressling@gressling.com>
2024-03-03 11:53:00 +0000

    REST to EDU system added

---
+++ python/cheminformatics/tools/imputer.py
@@ -0,0 +1,12 @@
+def impute(dataset: str, context: str = "") -> str:
+    """
+    Perform a imputation and returns the result.
+
+    Parameters:
+    - dataset (str): The first string to concatenate.
+    - context (str): Mask of the output (default: empty is like input)
+
+    Returns:
+    - str: The imputed string
+    """
+    return "not implemented"
