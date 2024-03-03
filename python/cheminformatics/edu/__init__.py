38303625b761aa8c029d5a1f46cb4a6d3b543264
Gressling <thorsten.gressling@gressling.com>
2024-03-03 12:08:18 +0000

    refactored to EDU

---
+++ python/cheminformatics/edu/__init__.py
@@ -0,0 +1,38 @@
+import requests
+
+class EDUInstance:
+    def __init__(self, apiKey, RESTURL="http://gressling.net/v2/"):
+        self.apiKey = apiKey
+        self.RESTURL = RESTURL
+
+    def connect(self):
+        """Checks connection to the API by verifying if the status code is 200."""
+        url = f"{self.RESTURL}api.php"
+        query = {"apiKey": self.apiKey}
+        try:
+            response = requests.get(url, params=query)
+        except Exception as e:
+            # Handling other exceptions
+            print(f"An error occurred: {e}")
+        else:
+            return response.status_code == 200
+
+    def testAccess(self):
+        """Retrieves data from the API if connection is successful."""
+        if self.connect():
+            url = f"{self.RESTURL}api.php"
+            query = {"apiKey": self.apiKey}
+            response = requests.get(url, params=query)
+            return response.json()
+        else:
+            return "Connection failed"
+
+    def testData(self):
+        """Retrieves data from the API if connection is successful."""
+        if self.connect():
+            url = f"{self.RESTURL}api.php"
+            query = {"apiKey": self.apiKey}
+            response = requests.get(url, params=query)
+            return response.json()
+        else:
+            return "Connection failed"
