844c98fe53dafbbc5587410d9e815413c9173aaa
Yakimochinai <89999567+Yakimochinai@users.noreply.github.com>
2024-09-24 00:47:53 +0200

    Delete python/cheminformatics zhao directory

--- python/cheminformatics zhao/edu/base.py
+++
@@ -1,4 +0,0 @@
-class BaseEDUInstance:
-    def __init__(self, apiKey, RESTURL):
-        self.apiKey = apiKey
-        self.RESTURL = RESTURL
--- python/cheminformatics zhao/utils.py
+++
@@ -1,8 +0,0 @@
-def hello():
-    print("Hello, World!")
-
-def main():
-    hello()
-
-if __name__ == "__main__":
-    main()
